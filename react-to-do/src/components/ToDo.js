2c2fbc9d065d07772a610a489548bd9b5135af2c
Ryan Manning <ryan.edward.manning@gmail.com>
2019-03-02 15:52:28 -0600

    Checkpoing 8 react-to-do-events: complete

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 
 class ToDo extends Component {
+  deleteTodo(description) {
+    this.props.deleteTodo(description);
+  }
+
   render () {
     return (
+      <div className="wrapper">
+
+      <button className="deleteTodo" onClick = {() =>
+        this.deleteTodo(this.props.description)}>Delete</button>
+
       <li>
-        <input type="checkbox" checked={this.props.isCompleted} />
-        <span>{this.props.description}</span>
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <span>{ this.props.description }</span>
       </li>
+      </div>
     );
   }
 }
+
 
 export default ToDo;
